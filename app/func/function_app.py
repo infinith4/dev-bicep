e5c344956471048724ae7833ccd37a421b1d7e1a
infinith4 <infinith4@gmail.com>
2024-09-28 23:20:03 +0900

    Update function_app.py

--- app/func/function_app.py
+++ app/func/function_app.py
@@ -4,14 +4,14 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.function_name(name="eventGridTrigger")
-@app.event_grid_trigger(arg_name="event")
-def event_grid_trigger(event: func.EventGridEvent):
-    """Process an event grid trigger for a new blob in the container."""
-    logging.info("Processing event %s", event.id)
+# @app.function_name(name="eventGridTrigger")
+# @app.event_grid_trigger(arg_name="event")
+# def event_grid_trigger(event: func.EventGridEvent):
+#     """Process an event grid trigger for a new blob in the container."""
+#     logging.info("Processing event %s", event.id)
 
 @app.function_name(name="eventGridTrigger01")
 @app.event_grid_trigger(arg_name="event")
-def event_grid_trigger01(event: func.EventGridEvent):
+def event_grid_trigger(event: func.EventGridEvent):
     """Process an event grid trigger for a new blob in the container."""
     logging.info("Processing event %s", event.id)
