8d4c914fcf954f241ec8fe2166aa71ef32a3bca1
infinith4 <infinith4@gmail.com>
2024-09-28 14:09:49 +0900

    Update function_app.py

--- app/func/function_app.py
+++ app/func/function_app.py
@@ -4,28 +4,8 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.event_grid_trigger(arg_name="azeventgrid")
-def EventGridTrigger01(azeventgrid: func.EventGridEvent):
-    logging.info('Python EventGrid trigger processed an event')
-
-
-@app.route(route="http_trigger02", auth_level=func.AuthLevel.FUNCTION)
-def http_trigger02(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+@app.function_name(name="eventGridTrigger")
+@app.event_grid_trigger(arg_name="event")
+def event_grid_trigger(event: func.EventGridEvent):
+    """Process an event grid trigger for a new blob in the container."""
+    logging.info("Processing event %s", event.id)
