851c62885543c21e2946439586edfef6d7126adf
infinith4 <infinith4@gmail.com>
2024-09-28 22:46:59 +0900

    Update function_app.py

--- app/func/function_app.py
+++ app/func/function_app.py
@@ -9,3 +9,9 @@ app = func.FunctionApp()
 def event_grid_trigger(event: func.EventGridEvent):
     """Process an event grid trigger for a new blob in the container."""
     logging.info("Processing event %s", event.id)
+
+@app.function_name(name="eventGridTrigger01")
+@app.event_grid_trigger(arg_name="event")
+def event_grid_trigger(event: func.EventGridEvent):
+    """Process an event grid trigger for a new blob in the container."""
+    logging.info("Processing event %s", event.id)
