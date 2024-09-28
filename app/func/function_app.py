9b88b45d05533fbf6b47b3912457ec2ef29e9ce6
infinith4 <infinith4@gmail.com>
2024-09-28 14:03:48 +0900

    Update function_app.py

--- app/func/function_app.py
+++ app/func/function_app.py
@@ -7,3 +7,25 @@ app = func.FunctionApp()
 @app.event_grid_trigger(arg_name="azeventgrid")
 def EventGridTrigger01(azeventgrid: func.EventGridEvent):
     logging.info('Python EventGrid trigger processed an event')
+
+
+@app.route(route="http_trigger02", auth_level=func.AuthLevel.FUNCTION)
+def http_trigger02(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
