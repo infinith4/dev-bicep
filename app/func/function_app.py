9e38433fd4684466f35b423c926d08a9192edb70
infinith4 <infinith4@gmail.com>
2024-09-29 07:04:02 +0900

    Update function_app.py

--- app/func/function_app.py
+++ app/func/function_app.py
@@ -15,3 +15,8 @@ app = func.FunctionApp()
 def event_grid_trigger(event: func.EventGridEvent):
     """Process an event grid trigger for a new blob in the container."""
     logging.info("Processing event %s", event.id)
+
+@app.schedule(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False)
+def myazfunction(myTimer: func.TimerRequest) -> None:
+    logging.info('Python timer trigger function starting.')
